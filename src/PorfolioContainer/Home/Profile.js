17aabda2076955a7f8d1c103330bc59e4d7237ca
jezreel gatmaitan <jezreelgatmaitan.wrk@gmail.com>
2022-06-21 12:47:52 +0800

    remove link

--- src/PorfolioContainer/Home/Profile.js
+++ src/PorfolioContainer/Home/Profile.js
@@ -9,19 +9,19 @@ export default function Profile() {
         <div className="profile-details">
           <div className="colz">
             <div className="cols-icon">
-              <a href="https://web.facebook.com/?_rdc=1&_rdr">
+              <a href="#">
                 <i className="fa fa-facebook-square" />
               </a>
               <a href="#">
                 <i className="fa fa-google-plus-square" />
               </a>
-              <a href="https://www.instagram.com/instructor_ehizeex/">
+              <a href="#">
                 <i className="fa fa-instagram" />
               </a>
-              <a href="https://www.youtube.com/channel/UCSSr5ZDFbilpZ592_ycoAwA">
+              <a href="#">
                 <i className="fa fa-youtube-square" />
               </a>
-              <a href="https://twitter.com/Ehiedu_baba">
+              <a href="#">
                 <i className="fa fa-twitter" />
               </a>
             </div>
